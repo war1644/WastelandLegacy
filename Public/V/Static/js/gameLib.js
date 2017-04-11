4a8d80526802a8e091b884ada40b716c4027158d
dxq <ahmerry@qq.com>
2017-04-11 22:41:30 +0800

    客户端基础CSS

--- Public/V/Static/js/gameLib.js
+++ Public/V/Static/js/gameLib.js
@@ -25,6 +25,34 @@ var Lib = function () {
     this.warning = '封装搞事情啦'
 };
 
+
+if (!window.localStorage) {
+    window.localStorage = {
+        getItem: function (sKey) {
+            if (!sKey || !this.hasOwnProperty(sKey)) { return null; }
+            return unescape(document.cookie.replace(new RegExp("(?:^|.*;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*((?:[^;](?!;))*[^;]?).*"), "$1"));
+        },
+        key: function (nKeyId) {
+            return unescape(document.cookie.replace(/\s*\=(?:.(?!;))*$/, "").split(/\s*\=(?:[^;](?!;))*[^;]?;\s*/)[nKeyId]);
+        },
+        setItem: function (sKey, sValue) {
+            if(!sKey) { return; }
+            document.cookie = escape(sKey) + "=" + escape(sValue) + "; expires=Tue, 19 Jan 2038 03:14:07 GMT; path=/";
+            this.length = document.cookie.match(/\=/g).length;
+        },
+        length: 0,
+        removeItem: function (sKey) {
+            if (!sKey || !this.hasOwnProperty(sKey)) { return; }
+            document.cookie = escape(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+            this.length--;
+        },
+        hasOwnProperty: function (sKey) {
+            return (new RegExp("(?:^|;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
+        }
+    };
+    window.localStorage.length = (document.cookie.match(/\=/g) || window.localStorage).length;
+}
+
 /**
  * 原型链继承扩展
  */
